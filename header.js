d9c4a32ea49c418e83010e2470f794c0495cb310
Bheta <65680585+TheBheta@users.noreply.github.com>
2025-01-24 23:37:50 -0500

    updated stuff

---
+++ header.js
@@ -0,0 +1,33 @@
+document.write(`
+<!-- Header -->
+<div id="header">
+
+    <!-- Inner -->
+        <div class="inner">
+            <header>
+                <h1><a href="index.html" id="logo">IBEC</a></h1>
+                <hr />
+                <p>University of Pennsylvania’s Premier Global Finance Club</p>
+            </header>
+        </div>
+
+    <!-- Nav -->
+        <a href="index.html"><img src="images/logo.png" id="nav-logo"></a>
+        <nav id="nav">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li>
+                    <a href="committees.html">Committees</a>
+                    <ul>
+                        <li><a href="committees.html#nac">Regional Committees</a></li>
+                        <li><a href="committees.html#coc">Corporate Outreach</a></li>
+                    </ul>
+                </li>
+                <li><a href="events.html">Events</a></li>
+                <li><a href="membership.html">Membership</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+
+</div>
+`)
