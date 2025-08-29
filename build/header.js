cf5e603a014852d05a55388cf2f646e1dbe4f4eb
Bheta <65680585+TheBheta@users.noreply.github.com>
2025-08-29 19:54:00 -0400

    update google analytics right this time

--- build/header.js
+++ build/header.js
@@ -31,11 +31,13 @@ document.write(`
     </nav>
 </div>
 <!-- Google Analytics -->
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKBH48M3XP"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
+
   gtag('config', 'G-TKBH48M3XP');
 </script>
 `);
